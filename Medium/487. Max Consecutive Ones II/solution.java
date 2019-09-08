09da3a902a9ded572a130527cbd6b06d79f0c969
Eric <ge.wen@husky.neu.edu>
2019-09-07 23:06:20 -0700

    solved the problem 487

---
+++ Medium/487. Max Consecutive Ones II/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0, zero = 0, k = 1; // flip at most k zero
+        for (int l = 0, h = 0; h < nums.length; h++) {
+            if (nums[h] == 0)
+                zero++;
+            while (zero > k) {
+                if (nums[l] == 0) {
+                    zero--;
+                }
+                l++;
+            }
+
+            max = Math.max(max, h - l + 1);
+        }
+        return max;
+    }
+}
