938bfbd71ba64374b3d025dc006785d7fb772632
wenhaoge <ge.wen@husky.neu.edu>
2019-11-04 13:33:38 -0800

    added the second solution

--- Medium/240. Search a 2D Matrix II/solution.java
+++ Medium/240. Search a 2D Matrix II/solution.java
@@ -40,3 +40,27 @@ class Solution {
         return false;
     }
 }
+
+/**
+ * 解法2:
+ */
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int col = matrix[0].length - 1;
+        int row = 0;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+}
