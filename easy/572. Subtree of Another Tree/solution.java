df0ec6093c4e941049dab18d6619f9c3bc81fe81
wenhaoge <ge.wen@husky.neu.edu>
2019-11-03 17:41:02 -0800

    come up with a new solution

--- easy/572. Subtree of Another Tree/solution.java
+++ easy/572. Subtree of Another Tree/solution.java
@@ -70,3 +70,31 @@ class Solution {
 
     }
 }
+
+// Solution 3
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+
+        if (s == null) {
+            return false;
+        }
+        if (isSame(s, t)) {
+            return true;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isSame(TreeNode node1, TreeNode node2) {
+
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        if (node1.val != node2.val) {
+            return false;
+        }
+        return isSame(node1.left, node2.left) && isSame(node1.right, node2.right);
+    }
+}
