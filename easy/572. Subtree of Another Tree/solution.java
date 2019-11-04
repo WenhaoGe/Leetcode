212aa9965d4295c592684f3d3475814162f2a4af
wenhaoge <ge.wen@husky.neu.edu>
2019-11-03 17:34:19 -0800

    added the second sloution for 572

--- easy/572. Subtree of Another Tree/solution.java
+++ easy/572. Subtree of Another Tree/solution.java
@@ -2,6 +2,8 @@
  * Definition for a binary tree node. public class TreeNode { int val; TreeNode
  * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
+
+// Solution 1:
 class Solution {
     public boolean isSubtree(TreeNode s, TreeNode t) {
         StringBuilder sb1 = new StringBuilder();
@@ -33,5 +35,38 @@ class Solution {
             sb.append("#lnull");
             sb.append("#rnull");
         }
+    }
+}
+
+// Solution 2:
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+
+        return traversal(s, t);
+    }
+
+    private boolean equal(TreeNode node1, TreeNode node2) {
+
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        if (node1.val == node2.val) {
+            return equal(node1.left, node2.left) && equal(node1.right, node2.right);
+        } else {
+            return false;
+        }
+    }
+
+    public boolean traversal(TreeNode s, TreeNode t) {
+
+        return s != null && (equal(s, t) || traversal(s.left, t) || traversal(s.right, t));
+
     }
 }
