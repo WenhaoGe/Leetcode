7e94a3844d042d4229f7c10bf0eaef7d868b35f1
wenhaoge <ge.wen@husky.neu.edu>
2019-11-05 21:48:46 -0800

    added the second soluion

--- Medium/572. Subtree of Another Tree/solution.java
+++ Medium/572. Subtree of Another Tree/solution.java
@@ -1,15 +1,10 @@
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
 class Solution {
     public boolean isSubtree(TreeNode s, TreeNode t) {
-        
+
         if (s == null) {
             return false;
         }
@@ -18,8 +13,9 @@ class Solution {
         }
         return isSubtree(s.left, t) || isSubtree(s.right, t);
     }
+
     private boolean isSame(TreeNode node1, TreeNode node2) {
-        
+
         if (node1 == null && node2 == null) {
             return true;
         }
@@ -30,5 +26,43 @@ class Solution {
             return false;
         }
         return isSame(node1.left, node2.left) && isSame(node1.right, node2.right);
+    }
+}
+
+/**
+ * Solution 2:
+ */
+
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        traversal(s, sb1);
+        traversal(t, sb2);
+        String s1 = sb1.toString();
+        String s2 = sb2.toString();
+        int len = s2.length();
+        for (int i = 0; i <= s1.length() - len; i++) {
+            if (s1.substring(i, i + len).equals(s2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void traversal(TreeNode s, StringBuilder sb) {
+
+        if (s != null) {
+            sb.append("#" + s.val);
+        }
+        if (s.left != null) {
+            traversal(s.left, sb);
+        }
+        if (s.right != null) {
+            traversal(s.right, sb);
+        } else {
+            sb.append("#lnull");
+            sb.append("#rnull");
+        }
     }
 }
