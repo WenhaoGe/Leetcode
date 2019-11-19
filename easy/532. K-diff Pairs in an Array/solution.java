691ac562931828e59be7d1bfb7227feb61c4842c
wenhaoge <ge.wen@husky.neu.edu>
2019-11-18 16:18:11 -0800

    added the second solution

--- easy/532. K-diff Pairs in an Array/solution.java
+++ easy/532. K-diff Pairs in an Array/solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
-        
+
         Arrays.sort(nums);
         int ans = 0;
         for (int i = 0; i < nums.length - 1; i++) {
@@ -11,6 +11,36 @@ class Solution {
                 if (Math.abs(nums[i] - nums[j]) == k) {
                     ans++;
                     break;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+/**
+ * Solution 2
+ */
+
+class Solution {
+    public int findPairs(int[] nums, int k) {
+
+        if (nums == null || nums.length == 0 || k < 0) {
+            return 0;
+        }
+        int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (k == 0) {
+                if (map.get(key) >= 2) {
+                    ans++;
+                }
+            } else {
+                if (map.containsKey(key + k)) {
+                    ans++;
                 }
             }
         }
