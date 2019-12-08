8017bd3a0fa9cf6c8f47783ff9ed67963c1e45a9
ge.wen@husky.neu.edu <ge.wen@husky.neu.edu>
2019-12-08 11:13:02 -0800

    added the second solution

--- easy/530. Minimum Absolute Difference in BST/solution.java
+++ easy/530. Minimum Absolute Difference in BST/solution.java
@@ -29,3 +29,27 @@ class Solution {
         traversal(list, root.right);
     }
 }
+
+/**
+ * solution 2:
+ */
+
+public class Solution {
+    
+    int minDiff = Integer.MAX_VALUE;
+    TreeNode prev;
+    
+    public int getMinimumDifference(TreeNode root) {
+        inorder(root);
+        return minDiff;
+    }
+    
+    public void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        if (prev != null) minDiff = Math.min(minDiff, root.val - prev.val);
+        prev = root;
+        inorder(root.right);
+    }
+
+}
