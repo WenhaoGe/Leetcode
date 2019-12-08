6b284e76b2584d108234eba27c343f3b7f190b2a
ge.wen@husky.neu.edu <ge.wen@husky.neu.edu>
2019-12-08 11:57:08 -0800

    added the new solution

--- Medium/701. Insert into a Binary Search Tree/solution.java
+++ Medium/701. Insert into a Binary Search Tree/solution.java
@@ -37,3 +37,38 @@ class Solution {
         inorder(list, root.right);
     }
 }
+
+/**
+ * solution 2
+ */
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        
+        build(root, val);
+        return root;
+    }
+    private void build(TreeNode root, int val) {
+        
+        if (root == null) {
+            return;
+        }
+        
+        if (root.val > val) {
+            if (root.left == null) {
+                root.left = new TreeNode(val);
+                return;
+            } else {
+                build(root.left, val);
+            }
+        } else if (root.val < val) {
+            if (root.right == null) {
+                root.right = new TreeNode(val);
+                return;
+            } else {
+                build(root.right, val);
+            }
+        }
+        return;
+    }
+}
