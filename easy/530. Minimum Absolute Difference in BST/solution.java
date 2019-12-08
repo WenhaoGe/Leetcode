7561acae5df544c6c39c5272988ac7ee5d77ad68
Wenhao Ge <wenhaoge@Wenhaos-MacBook-Pro.local>
2019-12-08 10:45:04 -0800

    solved the problem 530

---
+++ easy/530. Minimum Absolute Difference in BST/solution.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public int getMinimumDifference(TreeNode root) {
+        
+        if (root == null || (root.left == null && root.right == null)) {
+            return 0;
+        }
+        List<Integer> list = new ArrayList<>();
+        traversal(list, root);
+        int len = list.size();
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i < len; i++) {
+            ans = Math.min(ans, Math.abs(list.get(i) - list.get(i - 1)));
+        }
+        return ans;
+    }
+    
+    private void traversal(List<Integer> list, TreeNode root) {
+        
+        if (root == null) {
+            return;
+        }
+        traversal(list, root.left);
+        list.add(root.val);
+        traversal(list, root.right);
+    }
+}
