1414376dc97aae794d993bbc6c146b5b590fc5ef
ge.wen@husky.neu.edu <ge.wen@husky.neu.edu>
2019-12-12 15:28:17 -0800

    added the 2nd solution

--- easy/746. Min Cost Climbing Stairs/solution.java
+++ easy/746. Min Cost Climbing Stairs/solution.java
@@ -16,3 +16,23 @@ class Solution {
         return cost[index] + min;
     }
 }
+
+/**
+ * solution 2
+ */
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        
+        
+        int cost1 = cost[0];
+        int cost2 = cost[1];
+        int current = 0;
+        for (int i = 2; i < cost.length; i++) {
+            current = cost[i] + Math.min(cost1, cost2);
+            cost1 = cost2;
+            cost2 = current;
+        }
+        return Math.min(cost1, cost2);
+    }
+}
