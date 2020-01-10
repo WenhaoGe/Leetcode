c5dd288200b09c1e7da860adf9237a448ef293e5
ge.wen@husky.neu.edu <ge.wen@husky.neu.edu>
2020-01-09 17:39:57 -0800

    added the new solution

--- Medium/416. Partition Equal Subset Sum/solution.java
+++ Medium/416. Partition Equal Subset Sum/solution.java
@@ -40,3 +40,39 @@ class Solution {
         return arr[index][sum];
     }
 }
+
+/**
+
+ */
+
+class Solution {
+    public boolean canPartition(int[] nums) {
+        
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int each: nums) {
+            sum += each;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        return helper(nums, sum / 2, 0);
+    }
+    private boolean helper(int[] nums, int sum, int index) {
+        
+        if (sum == 0) {
+            return true;
+        }
+        if (index >= nums.length) {
+            return false;
+        }
+        if (sum >= nums[index]) {
+            if (helper(nums, sum - nums[index], index + 1)) {
+                return true;
+            }
+        }
+        return helper(nums, sum, index + 1);
+    }
+}
